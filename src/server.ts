270a57354e41c61081d769018d07967348218adf
VictorMeneghini <vhmeneghini@gmail.com>
2024-04-11 20:32:28 +0100

    chore: add prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,28 @@
 import { Server } from "@overnightjs/core";
-import { ForecastController } from "@src/controller/forecast"
-import "./util/module-alias"
+import { ForecastController } from "@src/controller/forecast";
+import "./util/module-alias";
 import express, { Application } from "express";
 
 export class SetupServer extends Server {
-
   constructor(private port = 3000) {
-    super()
+    super();
   }
 
   public init(): void {
-    this.setupExpress()
-    this.setupControllers()
+    this.setupExpress();
+    this.setupControllers();
   }
 
   private setupExpress(): void {
-    this.app.use(express.json())
+    this.app.use(express.json());
   }
 
   private setupControllers(): void {
-    const forecastController = new ForecastController()
-    this.addControllers([
-      forecastController
-    ])
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
   }
 
   public getApp(): Application {
-    return this.app
+    return this.app;
   }
 }
